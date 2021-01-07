6c996cb63d31c24b255484b3fe6a990892925a81
Samuel Alfredy Ambarita <samuelambarita2000@gmail.com>
2021-01-07 23:48:53 +0700

    Email Belum Fix

--- member_register.php
+++ member_register.php
@@ -1,6 +1,7 @@
 <?php
 	include 'koneksi.php';
 	include 'password.php';
+	include_once('smtp/PHPMailerAutoload.php');
 		$nama = $_POST['nama'];
 		$alamat = $_POST['alamat'];
 		$email = $_POST['email'];
@@ -21,6 +22,35 @@
 				if($hasil == NULL){
 					$new_pass = create_password($password);
 					mysqli_query($koneksi, "INSERT INTO users VALUES('', '$nama', '$alamat', '$email', '$nohp', '$username', '$new_pass', '$role')");
+					$html='Msg';
+					echo smtp_mailer('samuelambarita20@gmail.com','test',$html);
+					function smtp_mailer($to,$subject, $msg){
+						$mail = new PHPMailer(); 
+						$mail->SMTPDebug  = 3;
+						$mail->IsSMTP(); 
+						$mail->SMTPAuth = true; 
+						$mail->SMTPSecure = 'tls'; 
+						$mail->Host = "smtp.gmail.com";
+						$mail->Port = 587; 
+						$mail->IsHTML(true);
+						$mail->CharSet = 'UTF-8';
+						$mail->Username = "samuelambarita2000@gmail.com";
+						$mail->Password = "20092000a";
+						$mail->SetFrom("samuelambarita2000@gmail.com");
+						$mail->Subject = $subject;
+						$mail->Body =$msg;
+						$mail->AddAddress("samuelambarita20@gmail.com");
+						$mail->SMTPOptions=array('ssl'=>array(
+							'verify_peer'=>false,
+							'verify_peer_name'=>false,
+							'allow_self_signed'=>false
+						));
+						if(!$mail->Send()){
+							echo $mail->ErrorInfo;
+						}else{
+							return 'Sent';
+						}
+					}
 				echo "<script>alert('Daftar Berhasil $nama, Silahkan login'); window.location = 'login.php'</script>";
 				}
 				else
