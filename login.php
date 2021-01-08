fab9ba5f6a8327d2319562ccfe4f3e2dd9acd175
smanrung23 <smanrung23@gmail.com>
2021-01-08 14:08:39 +0700

    Perbaruan Captcha

--- login.php
+++ login.php
@@ -7,6 +7,10 @@
     <form action="login_act.php" method="POST">
     	<input type="text" name="username" placeholder="Username" />
   		<input type="password" name="password" placeholder="Password"/>
+      <div class="form-group">
+         <img src="generate.php" alt="gambar" style="width: 333px; height: 60px; margin-bottom:5px;"/>
+      </div>
+      <input type="text" name="kode" value="" placeholder="Kode Captcha" maxlength="5"/>   
   <input type="submit" value="Login"/>
   <div class="utilities">
     <a>Belum punya akun?</a>
